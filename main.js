cae376e7d44074f8876fbe716a5e789e3e55f440
Anjela Boyajy <anjelaboyajy@MacBook-Air.fios-router.home>
2021-11-28 23:38:13 -0500

    adding some html and js

---
+++ main.js
@@ -0,0 +1,4 @@
+const words = ["Georgia","Armenia","Russia","Thailand","Indonesia","Singapore","Ethiopia","Guinea","Kazakhstan","Tajikistan","Uzbekistan",];
+
+
+let secretWord = words[Math.floor(Math,random() * words.length)];
